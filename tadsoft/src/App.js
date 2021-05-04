ee1b01b37086e77067902f5371b9470df9630bb7
Neto <netodm10@gmail.com>
2021-05-04 07:30:31 -0400

    04/05 Chamada da Api dos pokemons funcionando

--- tadsoft/src/App.js
+++ tadsoft/src/App.js
@@ -1,23 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import{ getAllPokemons } from './services/pokemon';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevtUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const inicialUrl = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    async function fetchData(){
+      let response = await getAllPokemons(inicialUrl);
+      
+      console.log(response);
+
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      setLoading(false);
+    }
+
+    fetchData();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      { loading ? <h1>Loading...</h1> : (
+        <h1> PokeDados recebidos!</h1>
+      )}
     </div>
   );
 }
---
+++ tadsoft/src/services/pokemon.js
@@ -0,0 +1,9 @@
+export async function getAllPokemons(url){
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            resolve(data);
+        })
+    })
+}
