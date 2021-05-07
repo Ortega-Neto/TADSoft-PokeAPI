87c35a1e59dd26b55a61f885da2580aed848d735
Rafael Patricio <rodrigues0012@gmail.com>
2021-05-06 22:26:08 -0400

    v final css

--- tadsoft/src/App.js
+++ tadsoft/src/App.js
@@ -9,7 +9,6 @@ export default function App() {
   const [nextUrl, setNextUrl] = useState('');
   const [prevUrl, setPrevUrl] = useState('');
   const [loading, setLoading] = useState(true);
-  const [busca, setBusca] = useState('');
   const inicialUrl = 'https://pokeapi.co/api/v2/pokemon'
 
   useEffect(() => {
@@ -18,7 +17,8 @@ export default function App() {
       setNextUrl(response.next);
       setPrevUrl(response.previous);
 
-     
+      let pokenon = await loadPokemon(response.results);
+      console.log(pokenon);
 
       setLoading(false);
     }
@@ -46,8 +46,7 @@ export default function App() {
   }
 
   const loadPokemon = async (data) => {
-    console.log(data)
-;    let _pokemonData = await Promise.all(
+    let _pokemonData = await Promise.all(
       data.map(async pokemon =>{
         let responsePokemonData = await getPokemon(pokemon.url);
 
@@ -57,49 +56,17 @@ export default function App() {
 
     setPokemonData(_pokemonData);
   };
-  
-  async function searchByName(){
-    
-      setLoading(true);
-      setBusca(document.getElementById('buscaPoke').value);
-      let response = await getAllPokemons(inicialUrl+'/'+busca);
-      
-
-      let pokeS = await loadPokemon(response.results);
-      console.log(pokeS);
-
-      setLoading(false);
-    }
-  
-    
 
   return (
     <>
       <Navbar />
       <div>
-<<<<<<< HEAD
         {loading ? <h1 style={{ textAlign: 'center' }}>Buscando Pokemons...</h1> : (
           <>
             <div className="btn">
               <button onClick={prev}>Anterior</button>
               <button onClick={next}>Próxima</button>
-=======
-        {loading ? <h1 style={{ textAlign: 'center' }}>Carregando...</h1> : (
-          <>
-            <div className="btn">
-            <input type="text" placeholder="Buscar.." 
-              name="buscar" id="buscaPoke" onChange={event => setBusca(event.target.value)}></input>
-              <button type="submit" onClick={searchByName} ><i>Buscar</i></button>
             </div>
-            <div className="btn">
-              <button onClick={prev}>Anterior</button>
-              <button onClick={next}>Próximo</button>
-              
->>>>>>> b41338134876ef8d3134c8a6d4d3eb38e80bd5dc
-            </div>
-            
-            
-           
             <div className="grid-container">
               {pokemonData.map((pokemon, i) => {
                 return <PokemonCard key={i} pokemon={pokemon} />
@@ -107,12 +74,7 @@ export default function App() {
             </div>
             <div className="btn">
               <button onClick={prev}>Anterior</button>
-<<<<<<< HEAD
               <button onClick={next}>Próxima</button>
-=======
-              <button onClick={next}>Próximo</button>
-              
->>>>>>> b41338134876ef8d3134c8a6d4d3eb38e80bd5dc
             </div>
           </>
         )}
--- tadsoft/src/components/Navbar/Navbar.js
+++ tadsoft/src/components/Navbar/Navbar.js
@@ -3,18 +3,12 @@ import './style.css';
 
 export default function Navbar() {
     return (
-<<<<<<< HEAD
         
         
         <ul>
         
         <li >Pokédex</li>
         
-=======
-        <ul>
-        
-        <li>Pokédex</li>
->>>>>>> b41338134876ef8d3134c8a6d4d3eb38e80bd5dc
         
         </ul>
     );
